46bb788b73a9419b73c6c7eef21c21605450c672
LunaParajuliHerald <121007625+LunaParajuliHerald@users.noreply.github.com>
2023-03-21 21:36:43 +0545

    Add files via upload

---
+++ Workshop 3 ISA/script.js
@@ -0,0 +1,47 @@
+// var A = 5;
+// let a = 5 + 6;
+// console.log(a);
+// console.log(A);
+// let b = 6;
+// b = b + 10;
+// alert(b);
+// document.write("<br>" + "<p> Hello there!  </p>");
+// const c = 10;
+// alert(c);
+
+// function sum(a,b){
+//     return a+b;
+// }
+// var c = sum(4,7);
+// console.log(c);
+
+// var Sum = function (e, f){
+//     console.log(e + f);
+// }
+// Sum(9, 6);
+
+// const str = "Luna" + "Parajuli";
+// console.log(str);
+
+// var Sum = (e, f) => {
+//     return e + f;
+// }
+// var c = Sum(10,6);
+// console.log(c);
+
+// setTimeout(function (){
+//     console.log("Hello");
+// })
+// var fruits = ["apple", "banana", "cherry"];
+// fruits.forEach(function (fruit) {
+//     console.log(fruit);
+// });
+// (function () {
+//     console.log("hello");
+// })();
+
+let country = 'Nepal';
+let continent = 'Asia';
+let population = 90;
+ console.log("Name of my country is:" + country + " which lies in:" + continent + " and it has population of " + population + " millions."); 
+//  console.log(Name of my country is ${country})
